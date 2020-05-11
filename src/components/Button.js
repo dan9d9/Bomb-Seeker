ebf55e3b9a1cd9d444aceb6dff022cc8a22ccb0f
Daniel Dick <dan9d9@gmail.com>
2020-05-11 11:39:50 +0200

    remove some text

--- src/components/Button.js
+++ src/components/Button.js
@@ -19,8 +19,6 @@ const Button = props => {
 
 export default Button;
 
-// Icons made by <a href="https://smashicons.com/" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
-
 Button.propTypes = {
   idx: PropTypes.number.isRequired,
   mineNumber: PropTypes.oneOfType([
